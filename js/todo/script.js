8c518ac677fb34ddeb1569bf7aefc0bcd8947f00
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-02-21 12:04:21 +0530

    changes in todo app

--- js/todo/script.js
+++ js/todo/script.js
@@ -1,22 +1,25 @@
 const addbtn = document.getElementById("add-btn");
 const todoinput = document.getElementById("todo-input");
 const todocontainer = document.getElementById("todo-container");
-
+const themebtn = documen.getElementById("theme-button");
 addbtn.addEventListener("click", function () {
-  const value = todoinput.value.trim();
-  if (value === "") return;
-  const li = document.createElement("li"); // <li> </li>
-  li.innerHTML = value;
-  li.addEventListener("click", function () {
+  const value = todoinput.value;
+  if(value ==="") return;
+  const li= document.createElement("li");
+  li.innerHTML=value;
+  li.addEventListener('click',()=>{
     li.classList.toggle("completed");
-  });
+  })
   const delbtn = document.createElement("button");
-  delbtn.innerHTML = "❌";
-  delbtn.addEventListener("click", function () {
+  delbtn.innerHTML= "❌";
+  delbtn.addEventListener('click',()=>{
     li.remove();
-  });
-  
+  })
   li.appendChild(delbtn);
   todocontainer.appendChild(li);
-  todoinput.value = "";
+  todocontainer.value = "";
+  
 });
+themebtn.addEventListener("click",()=>{
+  document.body.classList.toggle("")
+}) 
