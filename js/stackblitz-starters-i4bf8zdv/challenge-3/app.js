1978a2a751b562915a4614b5ddd4f0e542c083d6
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-02-22 16:52:48 +0530

    challeneg 4 ongoing

--- js/stackblitz-starters-i4bf8zdv/challenge-3/app.js
+++ js/stackblitz-starters-i4bf8zdv/challenge-3/app.js
@@ -1,3 +1,23 @@
-/**
- * Write your challenge solution here
- */
+const nameInput = document.getElementById('nameInput');
+const jobInput = document.getElementById('jobInput');
+const ageInput = document.getElementById('ageInput');
+const bioInput = document.getElementById('bioInput');
+const nameDisplay = document.getElementById('nameDisplay');
+const jobDisplay = document.getElementById('jobDisplay');
+const ageDisplay = document.getElementById('ageDisplay');
+const bioDisplay = document.getElementById('bioDisplay');
+
+nameInput.addEventListener('input',function(){
+    nameDisplay.textContent = nameInput.value || "not provided"
+})
+jobInput.addEventListener('input',function(){
+    jobDisplay.textContent = jobInput.value || "not provided"
+})
+
+ageInput.addEventListener('input',function(){
+    ageDisplay.textContent = ageInput.value || "not provided"
+})
+
+bioInput.addEventListener('input',function(){
+    bioDisplay.textContent = bioInput.value || "not provided"
+})
--- js/stackblitz-starters-i4bf8zdv/challenge-4/app.js
+++ js/stackblitz-starters-i4bf8zdv/challenge-4/app.js
@@ -1,3 +1,9 @@
-/**
- * Write your challenge solution here
- */
+const taskinput= document.getElementById('taskInput');
+const addbtn = document.getElementById('addButton');
+const tasklist = document.getElementById('taskList');
+const totalTasks = document.getElementById('totalTasks');
+const completedTasks = document.getElementById('completedTasks');
+
+addbtn.addEventListener('click',()=>{
+   
+});
