5f9ca7ff10a5750007a699d93f76ab7ec7235b5d
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-02-28 21:34:56 +0530

    9th challenge done

--- js/js challenges/challenge-9/app.js
+++ js/js challenges/challenge-9/app.js
@@ -1,3 +1,13 @@
-/**
- * Write your challenge solution here
- */
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const menuPanel = document.querySelector(".panel");
+
+// Open the menu
+toggleBtn.addEventListener("click", () => {
+  menuPanel.classList.add("active");
+});
+
+// Close the menu
+closeBtn.addEventListener("click", () => {
+  menuPanel.classList.remove("active");
+});
