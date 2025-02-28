3b8f7b8ccc5d641f1fda0fbf659ed2e8d9629b66
abhishek sajwan <abhisheksajwan2004@gmail.com>
2025-02-28 12:15:21 +0530

    challenge 7 done

--- js/js challenges/challenge-7/app.js
+++ js/js challenges/challenge-7/app.js
@@ -1,1 +1,23 @@
-const
+const buttons = document.querySelectorAll(".accordion-button");
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const item = this.parentElement; // accordion-item
+    const content = item.querySelector(".accordion-content");
+
+    const isOpen = item.classList.contains("active");
+
+    document.querySelectorAll(".accordion-item").forEach((otherItem) => {
+      otherItem.classList.remove("active");
+      otherItem.querySelector(".accordion-content").style.maxHeight = "0px";
+      otherItem
+        .querySelector(".accordion-button")
+        .setAttribute("aria-expanded", "false");
+    });
+
+    if (!isOpen) {
+      item.classList.add("active");
+      content.style.maxHeight = content.scrollHeight + "px";
+      this.setAttribute("aria-expanded", "true");
+    }
+  });
+});
